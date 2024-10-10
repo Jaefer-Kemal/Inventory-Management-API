df524ce212b34410811762b50c7caf0998473a0c
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-10-10 14:28:28 +0300

    Added a Custom User model and also created a api folder

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser  
 
-# Register your models here.
+@admin.register(CustomUser)  
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "role",
+        "is_verified",
+        "date_joined"
+    )
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("role",)}),)
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("role",)}),  # Include the role field in the add form
+    )
+
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and return a regular user with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
+        extra_fields.setdefault('is_verified', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = [
+    ('admin', 'Admin'),
+    ('staff', 'Staff'),
+    ('store_manager', 'Store Manager'),
+    ('supplier', 'Supplier'),
+]
+
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True, blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='staff')
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)  # Indicates if the user has verified their role
+    date_joined = models.DateTimeField(default=timezone.now)
+
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'  # Use email as the unique identifier
+    REQUIRED_FIELDS = ["username"]  # Required fields when using createsuperuser
+
+    def __str__(self):
+        return f"{self.username} ({self.email})"
