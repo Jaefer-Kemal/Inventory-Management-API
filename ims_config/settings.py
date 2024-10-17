fa6ba3bedf429dde0bfca3aa7a45f9348e603ab7
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-10-17 15:56:24 +0300

    Modified allowed_host =[*]

--- ims_config/settings.py
+++ ims_config/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 from decouple import config
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://jaefer-ims-93716cb495ea.herokuapp.com/","127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
