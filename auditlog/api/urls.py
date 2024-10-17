e1c6923cea11368b797f6696182b778e04d1755a
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-10-17 12:07:20 +0300

    Modified the auditlog gave it serializer,views and url

--- auditlog/api/urls.py
+++ auditlog/api/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
+from .views import (
+    PurchaseOrderHistoryList,
+    PurchaseOrderHistoryDetail,
+    SalesOrderHistoryList,
+    SalesOrderHistoryDetail,
+    WarehouseStockHistoryList,
+    WarehouseStockHistoryDetail,
+)
 
 urlpatterns = [
-    
+    path('purchase-history/', PurchaseOrderHistoryList.as_view(), name='purchase-order-history-list'),
+    path('purchase-history/<int:pk>/', PurchaseOrderHistoryDetail.as_view(), name='purchase-order-history-detail'),
+    path('sales-history/', SalesOrderHistoryList.as_view(), name='sales-order-history-list'),
+    path('sales-history/<int:pk>/', SalesOrderHistoryDetail.as_view(), name='sales-order-history-detail'),
+    path('stock-history/', WarehouseStockHistoryList.as_view(), name='warehouse-stock-history-list'),
+    path('stock-history/<int:pk>/', WarehouseStockHistoryDetail.as_view(), name='warehouse-stock-history-detail'),
 ]
